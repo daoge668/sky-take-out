60447ca6faa07d54f1e00a54c276110a340f2f19
daoge <2318891066@qq.com>
2025-09-24 18:39:08 +0800

    用户端菜品套餐展示接口开发

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -3,6 +3,7 @@ package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -16,4 +17,8 @@ public interface DishFlavorMapper {
      * @param ids
      */
     void deleteByDishId(List<Long> ids);
+
+
+    @Select("select * from dish_flavor where dish_id = #{dishId}")
+    List<DishFlavor> getByDishId(Long id);
 }
